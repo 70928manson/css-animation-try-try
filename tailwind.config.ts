27a4438fc54d03ffecceb1bfaec8d68aef3076fc
70928manson <manson972@yahoo.com>
2024-06-04 14:20:22 +0800

    feat: 新增 tailwind 自訂 動畫設定

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,52 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        steam: {
+          '0%': { 
+            transform: 'translateY(0) scaleX(1)',
+            opacity: '0'
+          },
+          '15%': {
+            opacity: '1'
+          },
+          '50%': { transform: 'translateY(-150px) scaleX(5)' },
+          '95%': {
+            opacity: '0'
+          },
+          '100%': { transform: 'translateY(-300px) scaleX(10)' },
+        }
+      },
+      animation: {
+        'steam-up': 'steam 5s linear infinite'
+      },
+      //1 3 16 5 20 6 7 10 8 17 11 12 13 14 2 9 15 4 19
+      // *-500
+      animationDelay: {
+        1: "-500ms",
+        2: "-1000ms",
+        3: "-1500ms",
+        4: "-2000ms",
+        5: "-2500ms",
+        6: "-3000ms",
+        7: "-3500ms",
+        8: "-4000ms",
+        9: "-4500ms",
+        10: "-5000ms",
+        11: "-5500ms",
+        12: "-6000ms",
+        13: "-6500ms",
+        14: "-7000ms",
+        15: "-7500ms",
+        16: "-8000ms",
+        17: "-8500ms",
+        18: "-9000ms",
+        19: "-9500ms",
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animation-delay"),
+  ],
 };
 export default config;
