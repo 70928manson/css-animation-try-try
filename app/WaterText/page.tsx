fa5f6a828813c3fa1dc81dc546d165d5e654a79f
70928manson <manson972@yahoo.com>
2024-06-06 16:17:12 +0800

    fix: 修正中文text位置會亂跑

--- app/WaterText/page.tsx
+++ app/WaterText/page.tsx
@@ -4,11 +4,11 @@ const WaterText = () => {
   return (
     <div className="box-border font-['Noto_Sans_TC']">
       <div className="flex column justify-center items-center min-h-[100vh] bg-black">
-        <div className="relative w-full h-full">
-          <h2 className="absolute text-black translate-x-[125%] text-[8em]" style={{ color: 'transaprent', WebkitTextStroke: '2px #03a9f4' }}>
+        <div className="relative">
+          <h2 className="absolute text-black translate-x-[-50%] translate-y-[-50%] text-[8em]" style={{ wordBreak: 'keep-all', color: 'transaprent', WebkitTextStroke: '2px #03a9f4' }}>
             問題意識
           </h2>
-          <h2 className="absolute text-black translate-x-[125%] text-[8em] animate-water" style={{ color: '#03a9f4' }}>問題意識</h2>
+          <h2 className="absolute text-black translate-x-[-50%] translate-y-[-50%] text-[8em] animate-water" style={{ wordBreak: 'keep-all', color: '#03a9f4' }}>問題意識</h2>
         </div>
       </div>
     </div>
