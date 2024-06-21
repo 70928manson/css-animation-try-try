c0c3a03827ec6be9fc809986d5edc8662317f449
70928manson <manson972@yahoo.com>
2024-06-21 11:15:35 +0800

    fix: 修正deploy vercel bug

--- app/EyesFollowMouseCursor/component/Container.tsx
+++ app/EyesFollowMouseCursor/component/Container.tsx
@@ -1,19 +1,30 @@
 "use client";
 
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
+import Face from './Face';
 
-type ContainerProps = {
-    children?: React.ReactNode;
-    handleMouseMove: (e: React.MouseEvent) => void;
-};
+const Container: React.FC = () => {
+    const leftEyeRef = useRef<HTMLDivElement>(null);
+    const rightEyeRef = useRef<HTMLDivElement>(null);
 
-const Container: React.FC<ContainerProps> = ({ 
-    children,
-    handleMouseMove
-}) => {
+    const handleMouseMove = (e: React.MouseEvent) => {
+        refChange(e, leftEyeRef);
+        refChange(e, rightEyeRef);
+    }
+
+    const refChange = (e: React.MouseEvent, ref: React.RefObject<HTMLDivElement>) => {
+        if (ref.current) {
+            const x = (ref.current.getBoundingClientRect().left) + (ref.current.clientWidth / 2);
+            const y = (ref.current.getBoundingClientRect().top) + (ref.current.clientHeight / 2);
+            const radian = Math.atan2(e.pageX - x, e.pageY - y);
+            const rot = (radian * (180 / Math.PI)) * -1 + 270;
+            ref.current.style.transform = `rotate(${rot}deg)`;
+        }
+    }
+
     return (
         <div onMouseMove={(e) => handleMouseMove(e)} className="flex justify-center items-center min-h-[100vh] bg-[#5d3800]">
-            {children}
+            <Face leftEyeRef={leftEyeRef} rightEyeRef={rightEyeRef} />
         </div>
     );
 };
--- app/EyesFollowMouseCursor/page.tsx
+++ app/EyesFollowMouseCursor/page.tsx
@@ -1,32 +1,10 @@
 "use client";
 
-import React, { useRef } from 'react'
 import Container from './component/Container';
-import Face from './component/Face';
 
 const page = () => {
-  const leftEyeRef = useRef<HTMLDivElement>(null);
-  const rightEyeRef = useRef<HTMLDivElement>(null);
-
-  const handleMouseMove = (e: React.MouseEvent) => {
-    refChange(e, leftEyeRef);
-    refChange(e, rightEyeRef);
-  }
-
-  const refChange = (e: React.MouseEvent, ref: React.RefObject<HTMLDivElement>) => {
-    if (ref.current) {
-      const x = (ref.current.getBoundingClientRect().left) + (ref.current.clientWidth / 2);
-      const y = (ref.current.getBoundingClientRect().top) + (ref.current.clientHeight / 2);
-      const radian = Math.atan2(e.pageX - x, e.pageY - y);
-      const rot = (radian * (180 / Math.PI)) * -1 + 270;
-      ref.current.style.transform = `rotate(${rot}deg)`;
-    }
-  }
-
   return (
-    <Container handleMouseMove={handleMouseMove}>
-      <Face leftEyeRef={leftEyeRef} rightEyeRef={rightEyeRef} />
-    </Container>
+    <Container />
   )
 }
 
