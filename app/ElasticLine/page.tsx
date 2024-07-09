c76267b83b65df471f1710f3bafa0bece365ed67
70928manson <manson972@yahoo.com>
2024-07-09 16:24:22 +0800

    feat: 新增 elastic line page

---
+++ app/ElasticLine/page.tsx
@@ -0,0 +1,15 @@
+const page = () => {
+    return (
+        <div className="flex justify-center items-center min-h-[100vh] bg-[#000]">
+            <h2>Quick SVG Elastic Line Animation Effects | SVG CSS3</h2>
+            <svg className="absolute w-[400px] fill-none">
+                <path d="M0,25 C150,110 150,-60 300,25" stroke="#ff0092" strokeWidth={50}></path>
+            </svg>
+            <svg className="absolute w-[400px] fill-none">
+                <path d="M0,25 C150,110 150,-60 300,25" stroke="#ff0092" strokeWidth={50}></path>
+            </svg>
+        </div>
+    )
+}
+
+export default page
